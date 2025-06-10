7dd39a14ddb78233496822c9f2173c6dc855f948
Arief Badrus Sholeh <arifbadrus08@gmail.com>
2025-06-10 14:00:37 +0700

    feat create users api route test

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const createServer = require('./Infrastructures/http/createServer');
 const container = require('./Infrastructures/container');
+const config = require('./Commons/config');
 
 const start = async () => {
   const server = await createServer(container);
   await server.start();
   console.log(`server start at ${server.info.uri}`);
+  console.log(
+    `[INFO] Running in ${process.env.NODE_ENV} mode | DB: ${config.database.database} | PORT: ${config.app.port}`,
+  );
 };
 
 start();
